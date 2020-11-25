8d6b12e7fb45854cafe12c056a4cb9ba3387b496
Trung <dorapan2308@gmail.com>
2020-11-25 17:31:53 +0700

    load list

--- LaptopShop/Models/ProductDao.cs
+++ LaptopShop/Models/ProductDao.cs
@@ -16,5 +16,13 @@ namespace LaptopShop.Models
         {
             return db.Product.Where(x=>x.Catalog_ID==6).Take(quantity).ToList();
         }
+        public List<Product> getListProductMouse(int quantity)
+        {
+            return db.Product.Where(x => x.Catalog_ID == 5).Take(quantity).ToList();
+        }
+        public List<Product> getListProductSpeaker(int quantity)
+        {
+            return db.Product.Where(x => x.Catalog_ID == 3).Take(quantity).ToList();
+        }
     }
 }
